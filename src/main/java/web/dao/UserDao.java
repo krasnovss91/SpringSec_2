1cf25a77563a995773d7f10664ff57a688521519
krasnovss91 <krasnovstanislav91@gmail.com>
2020-11-30 20:53:27 +0400

    DAO

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,18 @@
+package web.dao;
+import web.model.User;
+import java.util.List;
+
+public interface UserDao {
+    User findUserByUsername(String username);
+
+    void saveUser(User user);
+
+    User getUserById(long id);
+
+    List<User> getAllUsers();
+
+    void editUser(User user);
+
+    void deleteUser(long id);
+
+}
