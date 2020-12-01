f32e17152069eb612678e7277bbd56d04ab2bbb5
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-01 09:18:47 +0400

    Service

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,15 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    public void saveUser(User user);
+    public User getUserById(long id);
+    public List<User> getAllUsers();
+    public void editUser(User user);
+    public void deleteUser(long id);
+    public boolean findUserByName(String name);
+}
+
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,83 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+import java.util.List;
+
+@Service("userDetailsService")
+public class UserServiceImpl implements UserService{
+
+
+    private UserDao userDao;
+
+    @Autowired
+    public void UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userDao.findUserByUsername(username);
+        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
+        if (user != null) {
+
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.disabled(!user.isEnabled());
+            builder.password(user.getPassword());
+            String[] authorities = user.getAuthorities()
+                    .stream().map(a -> a.getAuthority()).toArray(String[]::new);
+
+            builder.authorities(authorities);
+        } else {
+            throw new UsernameNotFoundException("User not found.");
+        }
+        return builder.build();
+    }
+
+    @Override
+    @Transactional
+    public boolean findUserByName(String name){
+        if (loadUserByUsername(name) == null)
+            return true;
+        else
+            return false;
+    }
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void editUser(User user) {
+        userDao.editUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(long id) {
+        userDao.deleteUser(id);
+    }
+
+}
+
