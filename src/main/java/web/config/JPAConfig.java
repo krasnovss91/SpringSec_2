ec97a7c3a132a27fdb18db37fd8d6687f244a5d7
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-07 21:40:22 +0400

    SessionFactory_1

--- src/main/java/web/config/JPAConfig.java
+++ src/main/java/web/config/JPAConfig.java
@@ -27,11 +27,19 @@ public class JPAConfig {
 
     @Bean
     public DataSource getDataSource() {
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+/*
         dataSource.setDriverClassName(env.getProperty("db.driver"));
         dataSource.setUrl(env.getProperty("db.url"));
         dataSource.setUsername(env.getProperty("db.username"));
         dataSource.setPassword(env.getProperty("db.password"));
+
+ */
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/db_demo?verifyServerCertificate=false&useSSL=false&requireSSL=false&useLegacyDatetimeCode=false&amp&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
         return dataSource;
     }
 
