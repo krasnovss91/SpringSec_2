96bc1735c959afe4827d3c6573acdcc6b1317a3c
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-10 12:31:52 +0400

    Service_1

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package web.service;
 
+import web.dao.UserDaoImpl;
 import web.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+
+    UserDaoImpl dao = new UserDaoImpl();
     @Override
     public void saveUser(User user) {
-
+      dao.saveUser(user);
     }
 
     @Override
@@ -22,12 +25,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void editUser(User user) {
-
+       dao.editUser(user);
     }
 
     @Override
     public void deleteUser(long id) {
-
+       dao.deleteUser(id);
     }
 
     @Override
