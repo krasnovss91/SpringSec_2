f75d794e4a45b90377ad0d52b8b95e0cd7a5eee5
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-14 16:24:47 +0400

    SF_5

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -4,12 +4,14 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+@Transactional
 @Repository
 public class UserDaoImpl implements UserDao {
 
