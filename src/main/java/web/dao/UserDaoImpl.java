20702ca1f35c301efa181c9e894cb6ab9822f9fa
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-17 14:08:33 +0400

    Edit_User_8

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -23,14 +23,14 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User findUserByUsername(String name) {
-        String hql =" FROM User WHERE username=:name";
-        Query query = sessionFactory.getCurrentSession().createQuery(hql).setParameter("name",name);
+        String hql = " FROM User WHERE username=:name";
+        Query query = sessionFactory.getCurrentSession().createQuery(hql).setParameter("name", name);
         return (User) query.getSingleResult();
     }
 
     @Override
     public void saveUser(User user) {
-     //   sessionFactory.getCurrentSession().save(user);
+        //   sessionFactory.getCurrentSession().save(user);
         if (user.getUsername() == null) {
             sessionFactory.getCurrentSession().save(user);
         }
@@ -50,6 +50,11 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void editUser(User user) {
+        try {
+            sessionFactory.getCurrentSession().update(user);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 /*
         String name = user.getUsername();
         User user1 = findUserByUsername(name);
@@ -58,23 +63,25 @@ public class UserDaoImpl implements UserDao {
    //     if (user.getUsername() != null) {//проблема в этой строке. Сравнить передаваемого в метод юзера с юзером из базы
             sessionFactory.getCurrentSession().save(user);
         }
- */
+
         Session session = sessionFactory.getCurrentSession();
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
-            session.update(user);
+           // session.update(user);
+            session.save(user);
             tx.commit();
             session.flush();
-
         } catch (RuntimeException e) {
             tx.rollback();
             e.printStackTrace();
         }
+         */
+
     }
 
     @Override
     public void deleteUser(String name) {
-      sessionFactory.getCurrentSession().delete(findUserByUsername(name));
+        sessionFactory.getCurrentSession().delete(findUserByUsername(name));
     }
 }
