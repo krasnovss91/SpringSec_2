25d296f3516388c762bea2149bf891090ffe36da
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-25 11:27:46 +0400

    Security_2

--- src/main/java/web/config/SecurityConfig.java
+++ src/main/java/web/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("ADMIN").password("ADMIN").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("ADMIN").password("ADMIN").roles("ADMIN");//изменить настройки здесь с inMemory  на наш сервис
     }
 
     @Override
