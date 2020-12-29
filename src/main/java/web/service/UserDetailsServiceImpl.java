23a805129850bb88916f72fc42e74136ccecb103
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-29 15:40:41 +0400

    Sec_Config_7

--- src/main/java/web/service/UserDetailsServiceImpl.java
+++ src/main/java/web/service/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import web.model.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,5 +15,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     UserService  userService;
+
+    @Override
+    public UserDetails findByUserName(String username){
+        User user = userService.findUserByName(username);
+        if(user.equals(null)){
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserDetailsServiceImpl(username);
+    }
 
 }
