aa18d7c40dd0d8ed61c8e7f0af8aecaf92decb22
krasnovss91 <krasnovstanislav91@gmail.com>
2021-01-07 22:39:07 +0400

    SuccessHandler_2

--- src/main/java/web/config/handler/LoginSuccessHandler.java
+++ src/main/java/web/config/handler/LoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package web.config.handler;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -16,21 +17,29 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException, ServletException {
-        httpServletResponse.sendRedirect("/admin");//перенаправляется на эту страницу в любом случае. Нужно сделать распознавание ролей здесь
-
-        /*
-           private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-  @Override
-  public void onAuthenticationSuccess(HttpServletRequest request,
-      HttpServletResponse response, Authentication authentication)
-      throws IOException, ServletException {
-        //set our response to OK status
-        response.setStatus(HttpServletResponse.SC_OK);
+       // httpServletResponse.sendRedirect("/admin");//перенаправляется на эту страницу в любом случае. Нужно сделать распознавание ролей здесь
 
         boolean admin = false;
 
-        logger.info("AT onAuthenticationSuccess(...) function!");
+        for(GrantedAuthority auth: authentication.getAuthorities()){
+            if("ADMIN".equals(auth.getAuthority())){
+                admin = true;
+            }
+        }
+
+        if(admin){
+            httpServletResponse.sendRedirect("/admin");
+        }else{
+            httpServletResponse.sendRedirect("/user");
+        }
+
+        /*
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)  throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        boolean admin = false;
 
         for (GrantedAuthority auth : authentication.getAuthorities()) {
             if ("ROLE_ADMIN".equals(auth.getAuthority())){
