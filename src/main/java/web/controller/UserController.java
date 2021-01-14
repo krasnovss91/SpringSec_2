6ee987176ee7a694bd9a1affdf2a7028fe53bc9b
krasnovss91 <krasnovstanislav91@gmail.com>
2021-01-14 14:33:11 +0400

    Logout_3

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -45,33 +45,14 @@ public class UserController {
         model.addAttribute("users", userService.getAllUsers());
         return "user";
     }
-/*
-    @GetMapping("/admin")
-    public String showUserForm(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("listUsers", userService.getAllUsers());
-        return "admin";
-    }
 
-    @PostMapping("admin/add")
-    public String addUser(@ModelAttribute User user) {
-
-        if (user.getUsername() != null) {
-            userService.saveUser(user);
-        } else {
-            userService.editUser(user);
-        }
-
-        return "redirect:/admin";
-    }
-*/
     @GetMapping("/register")
     public String registerForm(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("listUsers", userService.getAllUsers());
         return "register";
     }
-/*
+
     @PostMapping("admin/register")
     public String registerUser(@ModelAttribute User user) {
         if (user.getUsername() != null) {
@@ -83,28 +64,4 @@ public class UserController {
         return "redirect:/admin";
     }
 
-    @GetMapping("admin/edit/{username}")
-    public String editUser(@PathVariable("username") String username, Model model) {
-        model.addAttribute("user", userService.findUserByName(username));
-        return "edit-user";
-    }
-
-    @PostMapping("admin/edit/admin/edit")
-    public String editUser(@ModelAttribute("editUser") User user) {
-        userService.editUser(user);
-
-        return "redirect:/admin";
-    }
-
-    @RequestMapping("admin/delete/{username}")//так будет работать только с GET. без имени пользователя не работает
-    public String deleteUser(@PathVariable("username") String username) {
-   // @DeleteMapping("admin/delete")
-    //public String deleteUser(@ModelAttribute("deleteUser") User user){
-        userService.deleteUser(username);
-      //  userService.deleteUser(user);
-
-        return "redirect:/admin";
-    }
-    
- */
 }
