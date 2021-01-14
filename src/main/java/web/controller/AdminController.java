d452b6beca56adf6b1d2cd36a4c5a8178a657126
krasnovss91 <krasnovstanislav91@gmail.com>
2021-01-14 12:36:19 +0400

    AdminController_4

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -3,10 +3,7 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
@@ -22,14 +19,14 @@ public class AdminController {
         this.userService = userService;
     }
 
-    @GetMapping("/admin")
+    @GetMapping("/")
     public String showUserForm(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("listUsers", userService.getAllUsers());
         return "admin";
     }
 
-    @PostMapping("admin/add")
+    @PostMapping("/add")
     public String addUser(@ModelAttribute User user) {
 
         if (user.getUsername() != null) {
@@ -38,6 +35,35 @@ public class AdminController {
             userService.editUser(user);
         }
 
+        return "redirect:/admin";
+    }
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        if (user.getUsername() != null) {
+            userService.saveUser(user);
+        } else {
+            userService.editUser(user);
+        }
+
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/edit/{username}")
+    public String editUser(@PathVariable("username") String username, Model model) {
+        model.addAttribute("user", userService.findUserByName(username));
+        return "edit-user";
+    }
+
+    @PostMapping("/edit/admin/edit")
+    public String editUser(@ModelAttribute("editUser") User user) {
+        userService.editUser(user);
+
+        return "redirect:/admin";
+    }
+
+    @RequestMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username) {
+        userService.deleteUser(username);
         return "redirect:/admin";
     }
 }
