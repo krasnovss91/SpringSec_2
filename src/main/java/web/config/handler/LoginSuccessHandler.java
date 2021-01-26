c09a55da6551569acd5438c6ef374f5d0723160a
krasnovss91 <krasnovstanislav91@gmail.com>
2021-01-26 14:47:08 +0400

    RoleByName_2

--- src/main/java/web/config/handler/LoginSuccessHandler.java
+++ src/main/java/web/config/handler/LoginSuccessHandler.java
@@ -1,9 +1,12 @@
 package web.config.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import web.model.Role;
+import web.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,20 +17,33 @@ import java.rmi.AccessException;
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
+    private UserService userService;
+
+    @Autowired
+    public  LoginSuccessHandler(UserService userService){
+        this.userService = userService;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
-                                        Authentication authentication) throws IOException{
+                                        Authentication authentication) throws IOException {
+
+
 
         try {
 
             boolean admin = false;
+            Role role = userService.getRoleByName(authentication.getPrincipal().toString());
 
             for (GrantedAuthority auth : authentication.getAuthorities()) {
                 if ("ADMIN".equals(auth.getAuthority())) {
+                    //       String role = userService.getRoleByName(auth.getPrincipal)
+                    //    if("ADMIN".equals(role)){
                     admin = true;
                 }
             }
+
 
             if (admin) {
                 httpServletResponse.sendRedirect("/admin");
@@ -35,7 +51,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
                 httpServletResponse.sendRedirect("/user");
             }
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
 
         }
