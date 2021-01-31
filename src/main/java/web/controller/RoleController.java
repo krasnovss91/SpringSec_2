e98c1b0535a65a482f4e60dea492a0ee34ec47f1
krasnovss91 <krasnovstanislav91@gmail.com>
2021-01-31 15:48:53 +0400

    RoleController

---
+++ src/main/java/web/controller/RoleController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.service.RoleService;
+
+@Controller
+@RequestMapping("/")
+public class RoleController {
+    private RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService){
+        this.roleService = roleService;
+    }
+
+    @PostMapping("/roles")
+    public String getRoleByName(@PathVariable("name") String name){
+        roleService.findRoleByName(name);
+        return "redirect:/admin";
+    }
+}
