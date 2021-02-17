531d424067a01fcb41455cf63ba30646d9226cc9
krasnovss91 <krasnovstanislav91@gmail.com>
2021-02-17 16:48:34 +0400

    Login_24

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -16,10 +16,10 @@ public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
 
+
     @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
-
     }
 
     @Override
@@ -48,26 +48,15 @@ public class UserServiceImpl implements UserService {
     public void editUser(User user) {
 
         setUserRoles(user);
-        String password_1 = user.getPassword();
+        String password_1 = user.getPassword();//этот пароль надо как-то проверить на изменение
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        
-  //          long id = user.getId();
+
             String encodedPassword_1 = passwordEncoder.encode(password_1);
 //проверять пароль и шифровать только если меняется при редактировании. Не сравнивать пароль с самим собой
             user.setPassword(encodedPassword_1);//password_1 и encodedPassword1- не совпадают
         userDao.editUser(user);//проверить и шифровать пароль уже после занесения в бд?
-/*
-        User user1 = getUserById(id);
+        
 
-        String password_2 = user1.getPassword();
-
-        String encodedPassword_2 = passwordEncoder.encode(password_2);
-
-        if(password_1 != password_2){
-            user1.setPassword(encodedPassword_2);
-            userDao.editUser(user);
-        }
-*/
         
     }
 
@@ -80,8 +69,7 @@ public class UserServiceImpl implements UserService {
                 .collect(Collectors.toSet()));
     }
 
-
-
+    
     @Override
     public void deleteUser(long id) {
         userDao.deleteUser(userDao.getUserById(id).getId());
@@ -97,4 +85,6 @@ public class UserServiceImpl implements UserService {
     public Role getRoleByName(String name) {
         return userDao.getRoleByName(name);
     }
+
+
 }
